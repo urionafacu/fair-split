0e21e12f5c4ae9fcb2b6251a18def3a98f998b28
Facundo Uriona <urionafacu@gmail.com>
2024-10-02 22:28:25 -0300

    feat: add revalidate to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import ExpenseSplitter from "@/components/ExpenseSplitter";
 import { fetchIncomes } from "@/lib/services/incomeService";
 import { fetchExpenses } from "@/lib/services/expenseService";
 
+export const revalidate = 0;
+
 export default async function Home() {
   const [incomes, expenses] = await Promise.all([
     fetchIncomes(),
