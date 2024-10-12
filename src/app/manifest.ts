c94d64e18462331776d292d0bf13d9219c60d83d
Facundo Uriona <urionafacu@gmail.com>
2024-10-12 14:48:39 -0300

    feat: add pwa support

---
+++ src/app/manifest.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Fair Split",
+    short_name: "FaitSplit",
+    description: "A Progressive Web App for couples to manage shared expenses",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#fff",
+    icons: [
+      {
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-ico",
+      },
+    ],
+  };
+}
