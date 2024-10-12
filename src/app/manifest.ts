8ff6c25cfdeca662d9cb6a7a6a334b0c0929ab9b
Facundo Uriona <urionafacu@gmail.com>
2024-10-12 14:53:14 -0300

    fix(pwa): change shortname

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Fair Split",
-    short_name: "FaitSplit",
+    short_name: "FairSplit",
     description: "A Progressive Web App for couples to manage shared expenses",
     start_url: "/",
     display: "standalone",
