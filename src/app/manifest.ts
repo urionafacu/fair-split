502e8da7a513b50fd6696630ffbacb3eceec939f
Facundo Uriona <urionafacu@gmail.com>
2024-10-12 17:07:07 -0300

    fix: change metadata title and description

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#fff",
     icons: [
       {
-        src: "/favicon.png",
+        src: "/favicon.ico",
         sizes: "any",
         type: "image/x-ico",
       },
